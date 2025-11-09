80f35c898a50d83c4f730c651983684a928be31a
Mars-aliens <1367126980z@gmail>
2025-11-09 17:18:38 +0800

    Resolve CI conflicts by removing global side effects and correcting type casts in examples.

---
+++ docs/examples/tutorial/pure/cast.py
@@ -0,0 +1,9 @@
+T = cython.typedef(cython.p_int)
+
+cython.declare(t=cython.uint)
+
+t = 12345
+
+t1 = cython.cast(cython.size_t, cython.cast(T, t))
+
+t2 = cython.cast(cython.size_t, cython.cast(T, t, typecheck=True))
